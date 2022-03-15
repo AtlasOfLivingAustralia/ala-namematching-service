a08f1bbd65f75566c132f0dbeca43ae2f522a7fd
pal155 <Doug.Palmer@csiro.au>
2022-03-16 09:26:35 +1100

    Release 1.6

--- server/run.sh
+++ server/run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-mvn package && java -jar target/ala-namematching-server-1.6-SNAPSHOT.jar server config.yml
+mvn package && java -jar target/ala-namematching-server-1.6.jar server config.yml
 
